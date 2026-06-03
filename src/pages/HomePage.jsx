417b1ea9954fc942f395e38d3ea4c0fe32c5bbda
Shubham Narayankar <smnarayankar22@gmail.com>
2026-06-04 00:18:44 +0530

    Added filtering to homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useSelector, useDispatch } from "react-redux";
 import ProductCard from "../components/ProductCard";
 import { fetchProducts } from "../redux/slices/productsSlice";
@@ -6,13 +6,16 @@ import { fetchProducts } from "../redux/slices/productsSlice";
 
 function HomePage() {
   const dispatch = useDispatch();
-  
+  const [selectedCategory, setSelectedCategory] = useState("all");
 
   useEffect(() => {
     dispatch(fetchProducts())
   }, [dispatch]);
 
   const { products, status, error } = useSelector((state) => state.products)
+
+
+  const categories = ["all", ...new Set(products.map((product) => product.category))];
 
   if (status === "loading") {
     return <h1>Products are loading...Please Wait</h1>
@@ -21,14 +24,30 @@ function HomePage() {
   if (status === "failed") {
     return <h1>{error}</h1>
   }
-
   
+  const filteredProducts = selectedCategory === "all" ? products : products.filter((product)=>{
+    return product.category === selectedCategory
+  });
+
+
   return (
-    <div className="grid grid-cols-4 gap-4">
-      {products.map((product) => {
-        return <ProductCard key={product.id} product={product} />
-      })}
-    </div>
+    <>
+      <div>
+        <select
+          value={selectedCategory}
+          onChange={(event)=>setSelectedCategory(event.target.value)}
+        >
+          {categories.map((category, index)=>{
+            return <option key={index}>{category}</option>
+          })}
+        </select>
+      </div>
+      <div className="grid grid-cols-4 gap-4">
+        {filteredProducts.map((product) => {
+          return <ProductCard key={product.id} product={product} />
+        })}
+      </div>
+    </>
   );
 
 }
