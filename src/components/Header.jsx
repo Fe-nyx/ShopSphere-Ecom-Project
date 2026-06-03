9b745811230c3231505b84179352051f6b3bf7eb
Shubham Narayankar <smnarayankar22@gmail.com>
2026-06-03 13:24:13 +0530

    Added wishlist and cart counters

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,32 @@
 import { Link } from "react-router-dom"
 
-
+import { useSelector } from "react-redux"
 
 function Header() {
+
+  const cart = useSelector((state) => state.cart);
+  const wishlist = useSelector((state) => state.wishlist);
+
+  const cartTotal = cart.reduce((total, item) => {
+    return total + item.quantity
+  }, 0)
+
+  const wishlistTotal = wishlist.length;
+
+
+
 
   return (
     <>
       <h1>Header</h1>
       <Link to="/">Home</Link>
       <Link to="/login">Log In</Link>
-      <Link to="/wishlist">Wishlist</Link>
-      <Link to="/cart">Cart</Link>
+      <Link to="/wishlist">
+        Wishlist ({wishlistTotal})
+      </Link>
+      <Link to="/cart">
+        Cart ({cartTotal})
+      </Link>
     </>
   )
 }
--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,4 +1,7 @@
 
+
+
+
 function CartPage() {
 
   return (
